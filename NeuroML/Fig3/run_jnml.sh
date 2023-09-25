8c9f7a141b81079da3d5317a68aa9f115241d3ca
pgleeson <p.gleeson@gmail.com>
2023-09-25 15:24:42 +0100

    Disabling more tests

--- NeuroML/Fig3/run_jnml.sh
+++ NeuroML/Fig3/run_jnml.sh
@@ -6,7 +6,7 @@ python GeneratePopulationRate.py
 if [[ "$CI" != "true" ]]; then  # Throws out of memory error on GHA
     pynml LEMS_PopulationSimlowBaseline.xml  -nogui
     pynml LEMS_PopulationSimhighBaseline.xml -nogui
-fi
 
-pynml LEMS_PopulationSimlowBaseline.xml  -neuron -run -nogui
-pynml LEMS_PopulationSimhighBaseline.xml -neuron -run -nogui
+    pynml LEMS_PopulationSimlowBaseline.xml  -neuron -run -nogui
+    pynml LEMS_PopulationSimhighBaseline.xml -neuron -run -nogui
+fi
