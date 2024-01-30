0928947b8d28d5242881186aa91b8f0c6fe0ddd0
Padraig Gleeson <p.gleeson@gmail.com>
2024-01-30 11:31:53 +0000

    Run in background on nrn

--- NeuroML/Fig3/run_neuron_high_base_rate.sh
+++ NeuroML/Fig3/run_neuron_high_base_rate.sh
@@ -20,10 +20,27 @@ echo Removed LEMS_PopulationSimhighBaseline file
 # generate LEMS files
 python GeneratePopulationRate.py
 
-jnml LEMS_PopulationSimhighBaseline.xml -neuron
 
-# Generate folder with neuron executables
-nrnivmodl
+if [ "$1" == "-nogui" ]; then
 
-# On neuron with gui and run the simulation
-nrngui -python LEMS_PopulationSimhighBaseline_nrn.py
+    jnml LEMS_PopulationSimhighBaseline.xml -neuron -nogui
+
+    # Generate folder with neuron executables
+    nrnivmodl
+
+    echo Use neuron without gui and run the simulation
+    nrniv -python LEMS_PopulationSimhighBaseline_nrn.py
+
+else
+
+    jnml LEMS_PopulationSimhighBaseline.xml -neuron -nogui
+
+    # Generate folder with neuron executables
+    nrnivmodl
+
+    echo Use neuron with gui and run the simulation
+    nrngui -python LEMS_PopulationSimhighBaseline_nrn.py
+
+fi
+echo "Finished NEURON run"
+
--- NeuroML/Fig3/run_neuron_low_base_rate.sh
+++ NeuroML/Fig3/run_neuron_low_base_rate.sh
@@ -20,10 +20,26 @@ echo Removed LEMS_PopulationSimlowBaseline file
 # generate LEMS files
 python GeneratePopulationRate.py
 
-jnml LEMS_PopulationSimlowBaseline.xml -neuron
 
-# Generate folder with neuron executables
-nrnivmodl
+if [ "$1" == "-nogui" ]; then
 
-# On neuron with gui and run the simulation
-nrngui -python LEMS_PopulationSimlowBaseline_nrn.py
+    jnml LEMS_PopulationSimlowBaseline.xml -neuron -nogui
+
+    # Generate folder with neuron executables
+    nrnivmodl
+
+    echo Use neuron without gui and run the simulation
+    nrniv -python LEMS_PopulationSimlowBaseline_nrn.py
+
+else
+
+    jnml LEMS_PopulationSimlowBaseline.xml -neuron -nogui
+
+    # Generate folder with neuron executables
+    nrnivmodl
+
+    echo Use neuron with gui and run the simulation
+    nrngui -python LEMS_PopulationSimlowBaseline_nrn.py
+
+fi
+echo "Finished NEURON run"
